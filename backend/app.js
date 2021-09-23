865da51e2b775df0ebfa3e17a332accd9d061156
Javier Diez Garcia <fco.javier.diez.garcia@gmail.com>
2021-09-23 20:15:51 +0200

    Adding Game routingand model

--- backend/app.js
+++ backend/app.js
@@ -1,83 +1,50 @@
 var http = require('http'),
-    path = require('path'),
-    methods = require('methods'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cors = require('cors'),
-    errorhandler = require('errorhandler'),
-    mongoose = require('mongoose');
+	path = require('path'),
+	methods = require('method-override'),
+	express = require('express'),
+	errorhandler = require('errorhandler'),
+	cors = require('cors'),
+	slug = require('slug'),
+	mongoose = require('mongoose'),
+	uniqueValidator = require('mongoose-unique-validator');
 
-var isProduction = process.env.NODE_ENV === 'production';
-
-// Create global app object
+require('dotenv').config({ path: './config/config.env' });
 var app = express();
 
 app.use(cors());
-
-// Normal express config defaults
-app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(require('method-override')());
 app.use(express.static(__dirname + '/public'));
+app.use(errorhandler());
 
-app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+mongoose.connect(process.env.DB_MONGO_URI);
+mongoose.set('debug', true);
 
-if (!isProduction) {
-  app.use(errorhandler());
-}
+// require('./models/User');
+// require('./models/Article');
+// require('./models/Comment');
+// require('./config/passport');
 
-if(isProduction){
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
-}
-
-require('./models/User');
-require('./models/Article');
-require('./models/Comment');
-require('./config/passport');
-
-app.use(require('./routes'));
+// app.use(require('./routes'));
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
-
-    res.status(err.status || 500);
-
-    res.json({'errors': {
-      message: err.message,
-      error: err
-    }});
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({'errors': {
-    message: err.message,
-    error: {}
-  }});
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.json({
+		'errors': {
+			message: err.message,
+			error: {}
+		}
+	});
 });
 
-// finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, function(){
-  console.log('Listening on port ' + server.address().port);
+var server = app.listen(process.env.PORT || 3000, function () {
+	console.log('Listening on port ' + server.address().port);
 });
