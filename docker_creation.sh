ffc7052e9d9ecd5f16a8a0fcf9e629c4c1ab0810
Fco. Javier Diez García <75810726+JDiezGarcia@users.noreply.github.com>
2021-10-29 20:06:43 +0200

    Update docker_creation.sh

--- docker_creation.sh
+++ docker_creation.sh
@@ -22,19 +22,19 @@ checkContainer(){
 db(){
     if [ `docker ps -a -f name=$cDB | wc -l ` -eq 1 ] ;then
         docker build -t $cDB -f docker/dockerfile-db docker;
-        docker run --name=$cDB -v gameCheckVol:/data/db --network=$network -p 27017:27017 -d $cDB:latest
+        docker run --name=$cDB -v gameCheckVol:/data/db --network=$network -p 27017:27017 -d $cDB:latest;
     else
-        docker start $cDB > /dev/null &2>&1;;
+        docker start $cDB > /dev/null &2>&1;
     fi
 }
 
 server(){
     if [ $cExist -eq 1 ] || [ $cExist -eq 3 ];then
-        docker stop $cServer > /dev/null &2>&1;;
-        docker start $cServer > /dev/null &2>&1;;
+        docker stop $cServer > /dev/null &2>&1;
+        docker start $cServer > /dev/null &2>&1;
     else
         docker build -t $cServer -f docker/dockerfile-server docker;
-        docker run --name=$cServer -v ${path}/backend:/project --network=$network -p 4000:4000 -d $cServer:latest
+        docker run --name=$cServer -v ${path}/backend:/project --network=$network -p 4000:4000 -d $cServer:latest;
     fi
 }
 
@@ -44,13 +44,13 @@ client(){
         docker start $cClient > /dev/null &2>&1;
     else
         docker build -t $cClient -f docker/dockerfile-client docker;
-        docker run --name=$cClient -v ${path}/frontend:/project --network=$network -p 80:4200 -d $cClient:latest
+        docker run --name=$cClient -v ${path}/frontend:/project --network=$network -p 80:4200 -d $cClient:latest;
     fi
 }
 
 cExist=`checkContainer`;
 docker network create $network > /dev/null &2>&1;
-cp ./docker/config.env ./backend/config/
+cp ./docker/config.env ./backend/config/;
 
 db;
 server;
